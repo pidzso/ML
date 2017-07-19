16b0e478529e78fb527ae3b260185e960691be62
pidzso <balazs.pejo@gmail.com>
2017-07-19 15:33:10 +0200

    naive shrink

--- netflix_shrink.m
+++ netflix_shrink.m
@@ -7,13 +7,10 @@ z = full(train_vec(:, 3));
 [a, aa] = hist(x, unique(x));
 [b, bb] = hist(y, unique(y));
 
-clear q;
-
+% remove outliers
 ind = 0;
 while ind == 1
-
   aux = size(x);
-  
   for i=1:size(a, 2)
     if or(a(i)<50, a(i)>500)
       x(x == aa(i)) = 0;
@@ -25,7 +22,6 @@ while ind == 1
       z(i) = 0;
     end
   end
-
   for i=1:size(b, 2)
     if or(b(i)<100, b(i)>1000)
       y(y == bb(i)) = 0;
@@ -37,18 +33,34 @@ while ind == 1
       z(i) = 0;
     end
   end
-
   x(x == 0) = [];
   y(y == 0) = [];
   z(z == 0) = [];
-  
   if aux == size(x)
     ind = 0;
   end
-  
 end
+%train_vec = [x, y, z];
 
-train_vec = [x, y, z];
+% chose random users
+shrink_user = 0.33;
+rem_user = 1:max(x);
+rem_user = rem_user(randperm(max(x)));
+rem_user = rem_user(1:floor(max(x) * shrink_user));
+rem_x = ismember(x, rem_user) .* x;
+
+% chose random items
+shrink_item = 0.75;
+rem_item = 1:max(y);
+rem_item = rem_item(randperm(max(y)));
+rem_item = rem_item(1:floor(max(y) * shrink_item));
+rem_y = ismember(y, rem_item) .* y;
+
+% shrink dataset
+train_vec = [rem_x, rem_y, z];
+rem_train_vec = train_vec;
+rem_train_vec(any(train_vec==0,2),:) = [];
+train_vec = rem_train_vec;
 
 uniQ = sort(unique(train_vec(:,2)));
 all = train_vec(:, 2);
