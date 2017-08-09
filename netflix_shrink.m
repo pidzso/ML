e328e3f773ece532bdcdf3fa97cff68b2d258529
balazs.pejo <balazs.pejo@gmail.com>
2017-08-09 16:29:32 +0200

    preprocessing the input dataset

--- netflix_shrink.m
+++ netflix_shrink.m
@@ -4,44 +4,6 @@ x = full(train_vec(:, 1));
 y = full(train_vec(:, 2));
 z = full(train_vec(:, 3));
 
-[a, aa] = hist(x, unique(x));
-[b, bb] = hist(y, unique(y));
-
-% remove outliers
-ind = 0;
-while ind == 1
-  aux = size(x);
-  for i=1:size(a, 2)
-    if or(a(i)<50, a(i)>500)
-      x(x == aa(i)) = 0;
-    end
-  end
-  for i=1:size(x)
-    if x(i) == 0
-      y(i) = 0;
-      z(i) = 0;
-    end
-  end
-  for i=1:size(b, 2)
-    if or(b(i)<100, b(i)>1000)
-      y(y == bb(i)) = 0;
-    end
-  end
-  for i=1:size(y)
-    if y(i) == 0
-      x(i) = 0;
-      z(i) = 0;
-    end
-  end
-  x(x == 0) = [];
-  y(y == 0) = [];
-  z(z == 0) = [];
-  if aux == size(x)
-    ind = 0;
-  end
-end
-%train_vec = [x, y, z];
-
 % chose random users
 shrink_user = 0.33;
 rem_user = 1:max(x);
---
+++ preproc.m
@@ -0,0 +1,87 @@
+function [pr_d, pr_i, pr_u, pr_r, avg_u, avg_i] = preproc(data)
+  pr_d  = data; 
+  new_u = data(:, 1);
+  new_i = data(:, 2);
+  new_r = data(:, 3);
+%  avg_g = sum(new_d(:,3)) / n_rating;
+  [user_rat, users] = hist( new_u, unique(new_u));
+  [item_rat, items] = hist( new_i, unique(new_i));
+  
+  % remove outliers
+  item_bound = 10;
+  user_bound = 10;
+  
+  ind = 0;
+  while ind == 0
+    aux = size(pr_d);
+    
+    for i=1:size(item_rat, 2)
+      if item_rat(i)< item_bound
+        new_i(new_i == items(i)) = 0;
+      end
+    end
+    for i=1:size(new_i)
+      if new_i(i) == 0
+        new_u(i) = 0;
+        new_r(i) = 0;
+      end
+    end
+    
+    for u=1:size(user_rat, 2)
+      if user_rat(u) < user_bound
+        new_u(new_u == users(u)) = 0;
+      end
+    end
+    for u=1:size(new_u)
+      if new_i(u) == 0
+        new_u(u) = 0;
+        new_r(u) = 0;
+      end
+    end
+    
+    new_i(new_i == 0) = [];
+    new_u(new_u == 0) = [];
+    new_r(new_r == 0) = [];
+    pr_d = [new_u, new_i, new_r];
+    if aux == size(pr_d)
+        ind = 1;
+    end
+  end
+  
+  uniQ = sort(unique(pr_d(:, 1)));
+  all   = pr_d(:, 1);
+  for u = 1:size(uniQ)
+    all( all == uniQ(u) ) = u;
+  end
+  pr_d(:, 1) = all;
+  
+  uniQ = sort(unique(pr_d(:, 2)));
+  all   = pr_d(:, 2);
+  for i = 1:size(uniQ)
+    all( all == uniQ(i) ) = i;
+  end
+  pr_d(:, 2) = all;
+  
+  pr_u = max(pr_d(:, 1));
+  pr_i = max(pr_d(:, 2));
+  pr_r = size(pr_d, 1);
+  
+  [n_user_rat, n_users] = hist( new_u, unique(new_u));
+  [n_item_rat, n_items] = hist( new_i, unique(new_i));
+  
+  % calculate item/user averages
+  avg_i = n_items;
+  avg_u = n_users;
+  
+  for i=1:pr_i
+    aux = pr_d(pr_d(:, 2) == i, 3);
+    avg_i(i) = sum(aux) / n_item_rat(i);
+    pr_d(pr_d(:, 2) == i, 3) = aux - avg_i(i);
+  end
+  
+  for u=1:pr_u
+    aux = pr_d(pr_d(:, 1) == u, 3);
+    avg_u(u) = sum(aux) / n_user_rat(u);
+    pr_d(pr_d(:, 1) == u, 3) = aux - avg_u(u);
+  end 
+end
