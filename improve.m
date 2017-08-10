03afd17dd195602a06e3489efa7efe5149593b8d
pidzso <balazs.pejo@gmail.com>
2017-08-10 15:36:46 +0200

    Minor Updates

--- improve.m
+++ improve.m
@@ -1,6 +1,6 @@
 function [new_i, new_u] = improve(f_item, f_user, n_features, max_iter, ...
                           bound_f, bound_err, priv, n_item, n_user, ...
-                          lambda, epsilon, actual_g, gr_t_size, g)
+                          lambda, epsilon, actual_g, gr_t_size, g, mal_type)
       
   actual_u = double(actual_g(:, 1));
   actual_i = double(actual_g(:, 2));
@@ -8,6 +8,8 @@ function [new_i, new_u] = improve(f_item, f_user, n_features, max_iter, ...
 
     % compute predictions
     pred  = sum(f_item(actual_i, :) .* f_user(actual_u, :), 2);
+    
+    pred = bounding(pred, 1);
     
     % compute gradients
     err   = repmat(pred - actual_r, 1, n_features);
@@ -27,13 +29,13 @@ function [new_i, new_u] = improve(f_item, f_user, n_features, max_iter, ...
     clear reg_i;
     clear reg_u;
     
-    sens = bound_err * bound_f + lambda * bound_f;
-    grad_i = noise( ...
-             n_item, n_features, grad_i, max_iter, epsilon, sens, priv, g);
+    sens = epsilon * (bound_err * bound_f + lambda * bound_f);
+    grad_i = noise(n_item, n_features, grad_i, max_iter, ...
+                   epsilon, sens, mal_type, priv, g);
     
     % update features
     new_u = f_user - epsilon * grad_u;
-    new_i = f_item - epsilon * grad_i;
+    new_i = f_item - grad_i;
       
     clear grad_u;
     clear grad_i;
--- noise.m
+++ noise.m
@@ -1,10 +1,14 @@
-function new_grd = noise(n_item, n_features, grad_i, max_iter, epsilon, sens, priv, g)
-  if priv(g) > 0
+function new_grd = noise(n_item, n_features, grad_i, ...
+                         max_iter, epsilon, sens, mal_type, priv, g)
+  skip = 0; % first iterations
+  if and(strcmp(mal_type(g,:), 'pgd'), priv(g) > 0)
+    % http://nl.mathworks.com/matlabcentral/fileexchange/13705-laplacian-random-number-generator
     aux   = rand(n_item, n_features) - 0.5;
-    lap   = epsilon * n_features * n_item * max_iter * sens / ...
-            (priv(g) * sqrt(2)) * sign(aux).* log(1 - 2 * abs(aux));
-    new_grd = grad_i + lap;
+    sigma = n_features * n_item * (max_iter - skip) * sens / priv(g);
+    lap   = sigma / sqrt(2) * sign(aux).* log(1 - 2 * abs(aux));
+    new_grd = epsilon * (grad_i + lap);
   else
-    new_grd = grad_i;
+    new_grd = epsilon * grad_i;
   end
+    new_grd = bounding(new_grd, 0.1);
 end
